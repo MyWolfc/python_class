e37d7e8cf27e59060f232ea967b11da7e130d0d1
MyWolfc <juanitoamerica1@hotmail.com>
2023-09-18 10:21:01 -0600

    act

---
+++ unidad/Practica Clases/pr1.py
@@ -0,0 +1,25 @@
+import random
+
+#print(random.randint(2,5))
+
+listaOP = []
+for i in range(12):
+    listaOP.append(i)
+
+print(listaOP,len(listaOP))
+def crear_matriz(listaepica):
+    aux = int((len(listaepica)+2)/2)
+    print(aux)
+    auxi = 0
+    matrizEspejo = []
+    for i in range(aux):
+        if j%2 == 0:
+            pass
+        else:
+            pass
+
+        for j in range(aux):
+            pass
+    return matrizEspejo
+matrizepica = crear_matriz(listaOP)
+print(matrizepica)
